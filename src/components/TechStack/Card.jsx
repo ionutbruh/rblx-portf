e612fdd6616080e05b7601c74fb2f290ba0d5713
ionutbruh <jonutandrei@gmail.com>
2024-08-03 15:42:25 +0300

    mm

--- src/components/TechStack/Card.jsx
+++ src/components/TechStack/Card.jsx
@@ -1,10 +1,10 @@
-import React, { forwardRef } from 'react';
-
-const Card = forwardRef(({src, title }, ref) => (
-    <div className="flex flex-col items-center p-3 space-y-2" ref={ref}>
-        <img className='w-20 h-20' src={src} alt={title} />
-        <h3 className='font-vhs'>{title}</h3>
-    </div>
-));
+function Card({src, title}) {
+    return (
+        <div className="flex flex-col items-center">
+            <img className='w-20 h-20' src={src} alt={title} />
+            <h3 className='font-vhs'>{title}</h3>
+        </div>
+    );
+}
 
 export default Card;
--- src/components/TechStack/index.jsx
+++ src/components/TechStack/index.jsx
@@ -1,24 +1,11 @@
-import React, { useEffect, useRef } from 'react';
 import Card from './Card';
 import card_data from "./card_data";
-import anime from 'animejs';
 
 function TechStack() {
-    const cardsRef = useRef([]);
-
-    useEffect(() => {
-
-        anime({
-            targets: cardsRef.current,
-            translateY: -5,
-            delay: anime.stagger(100) // increase delay by 100ms for each elements.
-        });
-
-    }, []);
-
+ 
     return (
         <div className='bg-primary'>
-            <div className="flex flex-wrap justify-center p-10 space-x-10">
+            <div className="flex flex-wrap justify-center p-8 space-x-14">
                 {
                     card_data.map((data, index) => (
                         <Card
@@ -26,7 +13,6 @@ function TechStack() {
                             key={index}
                             src={data.src}
                             title={data.title}
-                            ref={el => cardsRef.current[index] = el}
                         />
                     ))
                 }
