c1f8ca584fca9679e524ff312a7555bf1f93f0ce
ionutbruh <jonutandrei@gmail.com>
2024-08-08 14:49:06 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import Home from "src/components/Home";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Home />
   );
 }
 
